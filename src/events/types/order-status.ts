eb26d548514f329e4a125d32ba929ee4650a269b
zgoksu <zafergoksu00@gmail.com>
2021-02-06 17:30:44 +0000

    Update common files

---
+++ src/events/types/order-status.ts
@@ -0,0 +1,17 @@
+export enum OrderStatus {
+    // When the order has been created, but the ticket
+    // it is trying to order has not been reserved.
+    CREATED = 'created',
+
+    // The ticket the order is trying to reserve has already
+    // been reserved, or when the user has cancelled the order.
+    // The order expires before payment.
+    CANCELLED = 'cancelled',
+
+    // The order has successfully reserved the ticket.
+    AWAITING_PAYMENT = 'awaiting:payment',
+
+    // The order has reserved the ticket and the user has
+    // provided payment successfully
+    COMPLETE = 'complete',
+}
